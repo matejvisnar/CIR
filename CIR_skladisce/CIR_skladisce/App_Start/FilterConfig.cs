56300d9113f4cf1184259a6076a9b2c24f67fd41
Boštjan <bostjan.janzovnik@gmail.com>
2015-06-11 09:40:25 +0200

    Prvi commit

---
+++ CIR_skladisce/CIR_skladisce/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace CIR_skladisce
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ CIR_skladisce/CIR_skladisce/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CIR_skladisce.MvcApplication" Language="C#" %>
