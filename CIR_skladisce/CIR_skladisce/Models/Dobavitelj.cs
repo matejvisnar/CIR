e4d7b2ea5e61fcbcfa52d4195361fa450b4505ec
Matej <visnarmatej91@gmail.com>
2015-06-15 11:11:37 +0200

    Modeli

---
+++ CIR_skladisce/CIR_skladisce/Models/Dobavitelj.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CIR_skladisce.Models
+{
+    public class Dobavitelj : Oseba
+    {
+        public int Id { get; set; }
+        public string Naziv { get; set; }
+        public string Email { get; set; }
+        public string Telefon { get; set; }
+
+        public Dobavitelj()
+        {
+
+        }
+    }
+}
---
+++ CIR_skladisce/CIR_skladisce/Models/Oseba.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CIR_skladisce.Models
+{
+    public class Oseba
+    {
+        public string Ime { get; set; }
+        public string Priimek { get; set; }
+        public string Davcna { get; set; }
+        public Naslov Naslov { get; set; }
+
+        public Oseba()
+        {
+            
+        }
+    }
+}
