7c83734fef36bbb8653742a19a7fd7bdd567f37a
Matej <visnarmatej91@gmail.com>
2015-06-15 19:57:02 +0200

    Uredi uporabnika, spremeni geslo

--- CIR_skladisce/CIR_skladisce/DeloSPodatki/DeloSPodatki.cs
+++ CIR_skladisce/CIR_skladisce/DeloSPodatki/DeloSPodatki.cs
@@ -35,7 +35,7 @@ namespace DbOperations
                                " FROM UPORABNIK u LEFT JOIN NASLOV n ON u.Naslov_id = n.id "+
                                " LEFT JOIN MESTO m "+
                                " ON n.mesto_id = m.id "+
-                               " WHERE u.id = @id ";
+                               " WHERE u.id = @id "; 
                 cmd.Parameters.Add("@id", ID);
                 MySqlDataReader rdr = cmd.ExecuteReader();
 
@@ -43,10 +43,13 @@ namespace DbOperations
                 while (rdr.Read())
                 {
                     Naslov naslov = new Naslov();
-                    naslov.Ulica =Convert.ToString(rdr[5]);
-                    naslov.HisnaSt = Convert.ToString(rdr[6]);
-                    naslov.Mesto = Convert.ToString(rdr[7]);
-                    naslov.PostnaSt =Convert.ToInt32(rdr[8]);
+                    if (Convert.ToString(rdr[5]) != "")
+                    {
+                        naslov.Ulica = Convert.ToString(rdr[5]);
+                        naslov.HisnaSt = Convert.ToString(rdr[6]);
+                        naslov.Mesto = Convert.ToString(rdr[7]);
+                        naslov.PostnaSt = Convert.ToInt32(rdr[8]);
+                    }
 
                     uporabnik.Id = Convert.ToInt32(rdr[0]);
                     uporabnik.Ime = Convert.ToString(rdr[1]);
@@ -55,14 +58,13 @@ namespace DbOperations
                     uporabnik.UporabniskoIme = Convert.ToString(rdr[4]);
                 }
                 rdr.Close();
-                return uporabnik;
                 povezava.zapriPovezavo();
+                return uporabnik;
             }
             catch (MySqlException ex)
             {
                 throw;
             }
-
         }
 
         #endregion
@@ -94,18 +96,127 @@ namespace DbOperations
 
         #region UPDATE
         /// <summary>
-        /// PRIMER
+        /// Uredi uporabnika
         /// </summary>
-        /// <param name="idPrijave"></param>
-        /*public void updateOdjavaZDogodka(int idPrijave)
+        /// 
+        public void updateUporabnik(Uporabnik uporabnik)
         {
             try
             {
-                DbPovezava povezava = new DbPovezava();
-                string sql = @"UPDATE prijava SET Deleted = 1 WHERE ID = " + idPrijave;
+                Naslov naslov = uporabnik.Naslov;
+                povezava.odpriPovezavo();
+
+                //Pridobi id drzave
+                MySqlCommand cmdPridobiIdDrzava = povezava.Connection.CreateCommand();
+                cmdPridobiIdDrzava.CommandText = "SELECT id from drzava WHERE naziv= @naziv ";
+                cmdPridobiIdDrzava.Parameters.Add("@naziv", naslov.Drzava);
+
+                MySqlDataReader rdrPridobiIdDrzava = cmdPridobiIdDrzava.ExecuteReader();
+                int idDrzava = 0;
+                while (rdrPridobiIdDrzava.Read())
+                {
+                    idDrzava = Convert.ToInt32(rdrPridobiIdDrzava[0]);
+                }
+                rdrPridobiIdDrzava.Close();
+
+                if (idDrzava == 0)
+                {
+                    MySqlCommand cmdInsertDrzava = povezava.Connection.CreateCommand();
+                    cmdInsertDrzava.CommandText = "INSERT INTO drzava(naziv) VALUES (@naziv)";
+                    cmdInsertDrzava.Parameters.Add("@naziv", naslov.Drzava);
+                    idDrzava = Convert.ToInt32(cmdInsertDrzava.LastInsertedId);
+                    cmdInsertDrzava.ExecuteNonQuery();
+                }
+
+                //Pridobi id mesto
+                MySqlCommand cmdPridobiIdMesto = povezava.Connection.CreateCommand();
+                cmdPridobiIdMesto.CommandText = "SELECT id from mesto WHERE kraj= @kraj ";
+                cmdPridobiIdMesto.Parameters.Add("@kraj", naslov.Mesto);
+
+                MySqlDataReader rdrPridobidMesto = cmdPridobiIdMesto.ExecuteReader();
+                int idMesto = 0;
+                while (rdrPridobidMesto.Read())
+                {
+                    idMesto = Convert.ToInt32(rdrPridobidMesto[0]);
+                }
+                rdrPridobidMesto.Close();
+
+                if (idMesto == 0)
+                {
+                    MySqlCommand cmdInsertMesto = povezava.Connection.CreateCommand();
+                    cmdInsertMesto.CommandText = "INSERT INTO mesto(kraj, posta) VALUES (@kraj,@posta)";
+                    cmdInsertMesto.Parameters.Add("@kraj", naslov.Mesto);
+                    cmdInsertMesto.Parameters.Add("@posta", naslov.PostnaSt);
+                    idMesto = Convert.ToInt32(cmdInsertMesto.LastInsertedId);
+                    cmdInsertMesto.ExecuteNonQuery();
+                }
+
+                //Uredi naslov
+                //Pridobi id drzave
+                MySqlCommand cmdPridobiNaslov = povezava.Connection.CreateCommand();
+                cmdPridobiNaslov.CommandText = "SELECT id from naslov WHERE ulica= @ulica AND hisna_st = @hisna_st ";
+                cmdPridobiNaslov.Parameters.Add("@ulica", naslov.Ulica);
+                cmdPridobiNaslov.Parameters.Add("@hisna_st", naslov.HisnaSt);
+
+                MySqlDataReader rdrPridobiIdNaslov = cmdPridobiIdDrzava.ExecuteReader();
+                int idNaslov = 0;
+
+                while (rdrPridobiIdNaslov.Read())
+                {
+                    idNaslov = Convert.ToInt32(rdrPridobiIdNaslov[0]);
+                }
+                rdrPridobiIdNaslov.Close();
+
+
+                if (idNaslov == 0)
+                {
+                    MySqlCommand cmdInsertNaslov = povezava.Connection.CreateCommand();
+                    cmdInsertNaslov.CommandText = "INSERT INTO naslov(mesto_id, Ulica, hisna_st) VALUES (@mesto_id, @ulica,@hisna_st)";
+                    cmdInsertNaslov.Parameters.Add("@mesto_id", idMesto);
+                    cmdInsertNaslov.Parameters.Add("@Ulica", naslov.Ulica);
+                    cmdInsertNaslov.Parameters.Add("@hisna_st", naslov.HisnaSt);
+                    idNaslov = Convert.ToInt32(cmdInsertNaslov.LastInsertedId);
+                    cmdInsertNaslov.ExecuteNonQuery();
+                }
+
+                MySqlCommand cmdUrediNaslov = povezava.Connection.CreateCommand();
+                cmdUrediNaslov.CommandText = "UPDATE Naslov SET mesto_id = @mesto_id, ulica = '@ulica', hisna_st = @hisna_st " +
+                                      " WHERE id = @id ";
+                cmdUrediNaslov.Parameters.Add("@mesto_id", idMesto);
+                cmdUrediNaslov.Parameters.Add("@ulica", naslov.Ulica);
+                cmdUrediNaslov.Parameters.Add("@hisna_st", naslov.HisnaSt);
+                cmdUrediNaslov.Parameters.Add("@id", naslov.Id);
+                cmdUrediNaslov.ExecuteNonQuery();
+
+                MySqlCommand cmd = povezava.Connection.CreateCommand();
+                cmd.CommandText = "UPDATE Uporabnik SET naslov_id = @naslov_id, ime = @ime, priimek = '@priimek', uporabnisko_ime = @uporabnisko_ime, davcna_st = @davcna_st " +
+                                  " WHERE u.id = @id ";
+                cmd.Parameters.Add("@naslov_id", idNaslov);
+                cmd.Parameters.Add("@ime", uporabnik.Ime);
+                cmd.Parameters.Add("@priimek", uporabnik.Priimek);
+                cmd.Parameters.Add("@uporabnisko_ime", uporabnik.UporabniskoIme);
+                cmd.Parameters.Add("@davcna_st", uporabnik);
+                cmd.Parameters.Add("@id", uporabnik.Id);
+                cmd.ExecuteNonQuery();
+
+                povezava.zapriPovezavo();
+            }
+            catch (MySqlException ex)
+            {
+                throw;
+            }
+        }
+
+        public void spremeniGeslo(int userId, string novoGeslo)
+        {
+            try
+            {
+                string sql = @"UPDATE uporabnik SET geslo = SHA1(@geslo) WHERE id = @id";
 
                 MySqlCommand cmd = new MySqlCommand(sql, povezava.Connection);
                 povezava.odpriPovezavo();
+                cmd.Parameters.Add("@geslo", novoGeslo);
+                cmd.Parameters.Add("@id", userId);
                 cmd.ExecuteNonQuery();
                 povezava.zapriPovezavo();
             }
@@ -113,7 +224,7 @@ namespace DbOperations
             {
                 throw;
             }
-        }*/
+        }
         #endregion
 
         #region AVENTIFIKACIJA
