8b1088c9b8e35845aedde2b7f5de5ecd0b809652
Boštjan <bostjan.janzovnik@gmail.com>
2015-06-15 10:36:44 +0200

    Dodajanje prvih razredov

---
+++ CIR_skladisce/CIR_skladisce/Models/Izdelek.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CIR_skladisce.Models
+{
+    public class Izdelek
+    {
+        public string Naziv { get; set; }
+        public string Opis { get; set; }
+        public decimal ProdajnaCena { get; set; }
+        public decimal OdkupnaCena { get; set; }
+        public int GarancijaMes { get; set; }
+
+        public Izdelek()
+        {
+
+        }
+
+
+    }
+}
